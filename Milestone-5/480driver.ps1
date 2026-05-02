3025fda6f21ccb4edb9a65dbb5268fce1a2b19cf
jnichollscyber <jnichollscyber@gmail.com>
2026-05-02 05:39:26 -0400

    Mileston 7 Complete

--- Milestone-5/480driver.ps1
+++ Milestone-5/480driver.ps1
@@ -6,19 +6,23 @@ $conf = Get-480Config -config_path "/home/jnicholls/SYS-480-Advanced-DevOps/Mile
 480Connect -server $conf.vcenter_server
 #Write-Host "Selecting your VM"
 #Select-VM -folder "BASE"
-$selected_vm = Select-VM -folder $conf.vm_folder
-cloneVM $conf $selected_vm
+#$selected_vm = Select-VM -folder $conf.vm_folder
+#cloneVM $conf $selected_vm
 #New-Network $conf
-#Set-Network
+Set-Network
 
 #Get-IP (Read-Host "Enter VM name")
+
+#Start-VMs -name "blue29-fw"
+
+
 
 #ChangeState
 
 # Connect
 #connectVCenter -server $config.vcenter_server
 
-# Linked Clone
+#Linked Clone
 #$newname = Read-Host "Enter new base name for VMs"
 #foreach ($i in 1..2) 
 #    {
