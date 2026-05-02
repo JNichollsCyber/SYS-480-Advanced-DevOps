f5d466756aa01eaff799ed39be212ea1b49eaaf1
jnichollscyber <jnichollscyber@gmail.com>
2026-05-02 08:51:18 -0400

    Fixed a bug

--- Milestone-9/ou.ps1
+++ Milestone-9/ou.ps1
@@ -2,9 +2,10 @@ Import-Module ActiveDirectory
 
 New-ADOrganizationalUnit -Name "Blue29" -Path "DC=blue29,DC=local"
 New-ADOrganizationalUnit -Name "Accounts" -Path "DC=blue29,DC=local"
-
-New-ADOrganizationalUnit -Name "Groups" -Path "OU=Accounts,OU=Blue29,DC=blue29,DC=local"
 New-ADOrganizationalUnit -Name "Computers" -Path "DC=blue29,DC=local"
 
+Start-Sleep -Seconds 10
+
+New-ADOrganizationalUnit -Name "Groups" -Path "OU=Accounts,OU=Blue29,DC=bluw29,DC=local"
 New-ADOrganizationalUnit -Name "Servers" -Path "OU=Computers,OU=Blue29,DC=blue29,DC=local"
 New-ADOrganizationalUnit -Name "Workstations" -Path "OU=Computers,OU=Blue29,DC=blue29,DC=local"
